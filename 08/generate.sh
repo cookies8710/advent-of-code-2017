5e1acf4e6c43087cd866a3b4c03d10f4d9b3e667
Tomas Surovec <tomas.surovec87@gmail.com>
2020-09-08 21:35:10 +0200

    08 part 2 - shell translator to C

--- 08/generate.sh
+++ 08/generate.sh
@@ -5,13 +5,14 @@ registers=$(cat <(cut -f1 -d' ' input) <(sed 's/.*if \([^ ]*\) .*/\1/' input) | 
 
 # register variable initialization
 echo "$registers" | sed 's/\(.*\)/int \1 = 0;/'
+echo "int total_max = 0, tmp=0;"
 
 # transform instructions
-sed -e 's/\(.*\)if\(.*\)/if (\2) \1;/' -e 's/inc/+=/' -e 's/dec/-=/' input
+sed -e 's/\(.*\)if\(.*\)/if (\2){tmp=\1;total_max=tmp>total_max?tmp:total_max;}/' -e 's/inc/+=/' -e 's/dec/-=/' input
 
 #echo "$registers" | sed 's/\(.*\)/printf("\1 = %d\\n", \1);/'
 
 echo "int all[] = {"
 echo "$registers" |  paste -s -d','
-echo '};int max=0;for(int i=0;i<sizeof(all)/sizeof(int);i++)if(max<all[i])max=all[i];printf("Part 1 result: %d\n",max);'
+echo '};int max=0;for(int i=0;i<sizeof(all)/sizeof(int);i++)if(max<all[i])max=all[i];printf("Part 1 result: %d\n",max);printf("Part 2 result: %d\n",total_max);'
 echo -e "return 0;\n}"
